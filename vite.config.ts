542cf54a6b461f85697e26b5dbc165ec1a7e0410
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-07-08 03:28:24 +0900

    set: 개발 환경 세팅

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,            // 개발 서버 포트 설정
+    strictPort: true       // 포트가 이미 사용중이면 에러 낸 후 종료
+  }
+})
