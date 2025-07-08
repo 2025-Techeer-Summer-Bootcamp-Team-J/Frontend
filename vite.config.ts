54380f63f369fa3a734348c2c7319b3e0a188b83
DHKim7762 <kim138762@gmail.com>
2025-07-08 15:52:47 +0900

    [Feat] 피부질환진단 1step 퍼블리싱 /#4 (#10)
    * feat: 피부질환 진단 1step 구현
    
    * feat: 피부질환 진단 1step 구현

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,            // 개발 서버 포트 설정
+    port: 5173,            // 개발 서버 포트 설정
     strictPort: true       // 포트가 이미 사용중이면 에러 낸 후 종료
   }
 })
