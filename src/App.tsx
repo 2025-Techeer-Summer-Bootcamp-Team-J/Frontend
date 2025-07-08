1ceb2e16645738556d1e1d1f26f9923de1dcd9ff
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-07-08 14:30:29 +0900

    feat: 피부질환 진단 1step 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 
 import { Routes, Route } from 'react-router-dom';
-import SkinAnalysisStep1 from './pages/SkinAnalysisStep1';
+import SkinAnalysisStep1 from './pages/DiseaseAnalysisStep1';
 import { GlobalStyle } from './styles/GlobalStyle';
 
 // HomePage 컴포넌트는 이제 필요 없으니 지워도 됩니다.
@@ -20,7 +20,7 @@ function App() {
 
         {/* --- 수정된 코드 --- */}
         {/* 기본 주소(path="/")로 접속하면 바로 SkinAnalysisStep1 페이지를 보여줌 */}
-        <Route path="/" element={<SkinAnalysisStep1 />} />
+        <Route path="/disease-analysis-step1" element={<SkinAnalysisStep1 />} />
 
         {/* 
           만약 /Skin 주소를 유지하고 싶다면 그대로 두어도 괜찮습니다.
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom' // 이 부분이 있는지 확인!
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* App을 감싸고 있는지 확인! */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
