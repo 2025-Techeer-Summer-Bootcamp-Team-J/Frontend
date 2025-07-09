f032ec808b16ed41bdc70ac03d1a9c52b01b55e9
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-07-09 16:39:38 +0900

    feat: clerk 소셜로그인 구현 및 테스트 완료

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,19 @@
+import { SignUp } from "@clerk/clerk-react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SignUpPage = () => {
+  return (
+    <PageContainer>
+      <SignUp path="/signup" routing="path" />
+    </PageContainer>
+  );
+};
+
+export default SignUpPage; 
---
+++ src/pages/SigninPage.tsx
@@ -0,0 +1,19 @@
+import { SignIn } from "@clerk/clerk-react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SignInPage = () => {
+  return (
+    <PageContainer>
+      <SignIn path="/signin" routing="path" />
+    </PageContainer>
+  );
+};
+
+export default SignInPage; 
