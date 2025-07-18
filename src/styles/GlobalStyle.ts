3eeea2f9e04aeb07466310be5dacf84b6af335f2
yumin <lxxwxntxr@gmail.com>
2025-07-18 17:36:02 +0900

    메인페이지 스크롤 모션 구현 (#61)

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -14,12 +14,20 @@ export const GlobalStyle = createGlobalStyle`
     html {
       /* 화면 너비에 따라 폰트 크기를 유동적으로 조절합니다. */
       /* 최소 14px, 기본 16px, 최대 18px (뷰포트 너비 360px ~ 1200px 기준) */
-      font-size: 80%;
+        font-size: 80%;
+        scroll-snap-type: y mandatory;
+        scroll-behavior: smooth;
+        overflow-y: scroll;
     }
 
     body {
         font-family: 'Noto Sans KR', sans-serif;
         background-color: white;
+        min-height: 100vh;
+    }
+
+    section {
+        scroll-snap-align: start;
         min-height: 100vh;
     }
 `;
