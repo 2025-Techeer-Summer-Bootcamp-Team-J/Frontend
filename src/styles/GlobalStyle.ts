64fc81e2f8ed0f380957efa252645371a08bbf43
yumin <lxxwxntxr@gmail.com>
2025-07-18 14:56:24 +0900

    메인페이지 비율 재수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
     html {
       /* 화면 너비에 따라 폰트 크기를 유동적으로 조절합니다. */
       /* 최소 14px, 기본 16px, 최대 18px (뷰포트 너비 360px ~ 1200px 기준) */
-      font-size: 75%;
+      font-size: 80%;
     }
 
     body {
