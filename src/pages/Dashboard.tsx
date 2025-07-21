e24368f609081674b45be909f64d2b3185772ece
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-07-21 17:07:19 +0900

    빌드 오류 해결

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../services';
 import type { DashboardData } from '../services/types';
 import styled, { createGlobalStyle } from 'styled-components';
