228f0db447f89a90edcaed406d4ce0d74d1c3271
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-07-24 00:39:34 +0900

    엣지 브라우저도 가능하게 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+/// <reference types="node" />
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -22,9 +23,10 @@ export default defineConfig({
     strictPort: true,      // 포트가 이미 사용중이면 에러 낸 후 종료
     proxy: {
       '/api': {
-        target: 'VITE_API_BASE_UR',  // 백엔드 서버 포트 추가
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:8000', // 백엔드 URL
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
